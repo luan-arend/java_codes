0aa7c256b4b4b8f69ac65273770948e9a0e4c448
luan-arend <luan00luchie@hotmail.com>
2024-09-28 16:05:13 -0300

    aulas de construtores, arraylist e toString

---
+++ 0. Examples/ArrayList/src/examples/arraylist/model/Pessoa.java
@@ -0,0 +1,16 @@
+package examples.arraylist.model;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa: " + nome + " (Idade: " + idade + ")";
+    }
+}
