226a48edcaee6793bda59b110a8f529963ed5991
luan-arend <luan00luchie@hotmail.com>
2024-10-22 20:55:19 -0300

    Implementando lib Gson para ler e organizar retornos do json

--- 0. Examples/Request HTTP/httpRequest.java
+++ 0. Examples/Request HTTP/httpRequest.java
@@ -1,14 +1,23 @@
+import com.google.gson.Gson;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class httpRequest {
     public static void main (String[] args) throws IOException, InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um filme para busca: ");
+        var busca = scanner.nextLine();
+
+        var URL = "https://www.omdbapi.com/?t=" + busca + "&apikey=94094c90";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=matrix&apikey=94094c90"))
+                .uri(URI.create(URL))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
--- 4. Java consumindo API, gravando arquivos e lidando com erros/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ 4. Java consumindo API, gravando arquivos e lidando com erros/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -1,19 +1,44 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.modelos.Titulo;
+import br.com.alura.screenmatch.modelos.TituloOmdb;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main (String[] args) throws IOException, InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um filme para busca: ");
+        var busca = scanner.nextLine();
+
+        var URL = "https://www.omdbapi.com/?t=" + busca + "&apikey=94094c90";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=matrix&apikey=94094c90"))
+                .uri(URI.create(URL))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+
+        String json = response.body();
+        System.out.println(json);
+
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .create();
+        TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+        System.out.println(meuTituloOmdb);
+        Titulo meuTitulo = new Titulo(meuTituloOmdb);
+        System.out.println(meuTitulo);
+
+
     }
 }
